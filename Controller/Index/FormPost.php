b884f3d3be976064d13637bd2ebe6fc040034a3b
Ryan <ryan.trung@omnyfy.com>
2022-12-30 02:06:37 +0700

    update/add new post

--- Controller/Index/FormPost.php
+++ Controller/Index/FormPost.php
@@ -14,15 +14,18 @@ class FormPost extends \Magento\Framework\App\Action\Action
 {
     protected $postFactory;
     protected $postResourceModel;
+    protected $resultFactory;
 
     public function __construct(
         Context $context,
+        \Magento\Framework\Controller\ResultFactory $resultFactory,
         \Neo\Demo\Model\PostFactory $postFactory,
         \Neo\Demo\Model\ResourceModel\Post $postResourceModel
     ) {
         parent::__construct($context);
         $this->postFactory = $postFactory;
         $this->postResourceModel = $postResourceModel;
+        $this->resultFactory = $resultFactory;
     }
 
     /**
@@ -33,5 +36,9 @@ class FormPost extends \Magento\Framework\App\Action\Action
     public function execute()
     {
         $postValue = $this->getRequest()->getPostValue();
+        $post = $this->postFactory->create()->setData($postValue);
+        $post->save();
+        $redirect = $this->resultFactory->create(\Magento\Framework\Controller\ResultFactory::TYPE_REDIRECT);
+        return $redirect->setUrl('/demo/index/index/');
     }
 }
