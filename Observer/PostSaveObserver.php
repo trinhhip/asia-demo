ea28e098c17f4bce4c956a6543c8f14b4ed26a6b
Neo Doan <doan.trinh@banago.shop>
2022-12-30 14:47:38 +0700

    post save event

--- Observer/PostSaveObserver.php
+++ Observer/PostSaveObserver.php
@@ -3,9 +3,17 @@ namespace Neo\Demo\Observer;
 
 class PostSaveObserver implements \Magento\Framework\Event\ObserverInterface
 {
+    protected $resourceConnection;
+
+    public function __construct(
+        \Magento\Framework\App\ResourceConnection $resourceConnection
+    ) {
+        $this->resourceConnection = $resourceConnection;
+    }
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         $post = $observer->getEvent()->getPost();
-        $a = 1;
+        $conn = $this->resourceConnection->getConnection();
+        $conn->insert('notify_to_author', ['post_id' => $post->getId()]);
     }
 }
